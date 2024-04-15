448452cb2cc69dcf3f91d3a5f73e0c0c6977a11d
gandroide <ajontiveros82@gmail.com>
2024-04-15 15:22:12 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth <= 600) {
+        console.log("La pantalla es pequeña, puedes hacer algo aquí.");
+    } else {
+        console.log("La pantalla es grande, puedes hacer algo diferente aquí.");
+    }
+});
